4a29aa0c3a1c91c4b88682bb33118615ec591a4e
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-02-18 20:31:19 +0700

    fix responsive

--- Asset/script/animation.js
+++ Asset/script/animation.js
@@ -16,14 +16,14 @@ ScrollReveal().reveal(".nav__links", {
 ScrollReveal().reveal(".hero__title", {
    opacity: 0,
    delay: 300,
-   duration: 700,
+   duration: 600,
    reset: true,
 });
 
 ScrollReveal().reveal(`#hs1,#hs2,#hs3`, {
    origin: "top",
    distance: "700px",
-   duration: 1000,
+   duration: 900,
    opacity: 0,
    reset: true,
 });
@@ -31,7 +31,7 @@ ScrollReveal().reveal(`#hs1,#hs2,#hs3`, {
 ScrollReveal().reveal(`.about__images`, {
    origin: "left",
    distance: "500px",
-   duration: 1000,
+   duration: 900,
    opacity: 0,
    reset: true,
 });
@@ -39,7 +39,7 @@ ScrollReveal().reveal(`.about__images`, {
 ScrollReveal().reveal(`.about__content__container`, {
    origin: "right",
    distance: "500px",
-   duration: 1000,
+   duration: 900,
    opacity: 0,
    reset: true,
 });
@@ -52,7 +52,7 @@ ScrollReveal().reveal(`.section__title__container`, {
 
 for (let i = 1; i <= 6; i++)
    ScrollReveal().reveal(`#sv${i}`, {
-      duration: 1000,
+      duration: 900,
       scale: 0,
       reset: true,
       delay: 300 + i * 200,
@@ -60,7 +60,7 @@ for (let i = 1; i <= 6; i++)
 
 for (let i = 1; i <= 4; i++)
    ScrollReveal().reveal(`#ms${i}`, {
-      duration: 1000,
+      duration: 900,
       scale: 0,
       reset: true,
       delay: 300 + i * 200,
@@ -78,13 +78,13 @@ for (let i = 1; i <= 10; i++)
 ScrollReveal().reveal(`.footer__read`, {
    origin: "top",
    distance: "500px",
-   duration: 1000,
+   duration: 900,
    opacity: 0,
    reset: true,
 });
 
 ScrollReveal().reveal(`.footer__content`, {
-   scale: 1.5,
+   scale: 1.6,
    duration: 1500,
    opacity: 0,
    reset: true,
